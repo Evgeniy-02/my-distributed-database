1c8536dcf4db855bc10fb42b2b7cd0602cc28478
Svetlana <pirozhkova@ecopsy.ru>
2023-05-01 18:16:57 +0300

    Divider by count changed on id. Removed TODO

--- MainNode/src/main/java/ru/bmstu/labs/MainNode/service/NodeService.java
+++ MainNode/src/main/java/ru/bmstu/labs/MainNode/service/NodeService.java
@@ -1,6 +1,5 @@
 package ru.bmstu.labs.MainNode.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpEntity;
@@ -16,98 +15,136 @@ import ru.bmstu.labs.MainNode.issue.LabServiceException;
 import ru.bmstu.labs.MainNode.model.User;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
 public class NodeService {
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
+    //    @Autowired
+    private final DiscoveryClient discoveryClient;
+    private final RestTemplate restTemplate;
+    private final HttpHeaders headers;
 
     private List<ServiceInstance> nodes;
-    private static int counter = 0;
+//    private static int counter = 0;
 
-    final RestTemplate restTemplate;
-    final HttpHeaders headers;
-
-    public NodeService() {
+    public NodeService(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
         restTemplate = new RestTemplate();
         headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
     }
 
-    private String currentNodeUrl(boolean updateNodeList) {
-        if (updateNodeList)
-            updateNodeList();
-        return ((ServiceInstance) nodes.get(counter)).getUri().toString();
-    }
+    /*private <R> User sendRequest(String urlPath, R request) throws LabServiceException {
+        HttpEntity<R> httpRequest = new HttpEntity<>(request, headers);
 
-    private String nextNodeUrl() {
-        updateNodeList();
-        counter = ++counter % nodes.size();
-        return currentNodeUrl(false);
-    }
-
-    private <R> User sendRequest(String urlPath, R request) throws LabServiceException {
-        HttpEntity httpRequest = new HttpEntity<R>(request, headers);
-
-        String url = currentNodeUrl(true) + urlPath;
+        String url = getOriginalNodeUri() + urlPath;
         User user = restTemplate.postForObject(url, httpRequest, User.class);
         if (user == null)
             throw new LabServiceException("Received null in " + this + " from " + url);
 
         return user;
-    }
+    }*/
 
     public List<User> getEntities(String alias) {
         updateNodeList();
 
         List<User> resultList = new ArrayList<>();
 
-        for (int i = 0; i < nodes.size(); i++) {
-            HttpEntity httpRequest = new HttpEntity<String>(alias, headers);
+        for (ServiceInstance node : nodes) {
+            HttpEntity<String> httpRequest = new HttpEntity<>(alias, headers);
 
-            String url = ((ServiceInstance) nodes.get(i)).getUri().toString() + "/users";
+            String url = node.getUri().toString() + "/users";
             User[] users = restTemplate.postForObject(url, httpRequest, User[].class);
 
-            resultList.addAll(List.of(users));
+            resultList.addAll(List.of(users != null ? users : new User[0]));
         }
 
         return resultList;
 
     }
 
-    // TODO for all connected nodes
     public User getEntity(UserGetRequest request) throws LabServiceException {
-        return sendRequest("/user", request);
+        HttpEntity<UserGetRequest> httpRequest = new HttpEntity<>(request, headers);
+
+        // Main
+        String originalUrl = getOriginalNodeUri(request.getId()) + "/user";
+        User user = restTemplate.postForObject(originalUrl, httpRequest, User.class);
+        if (user == null)
+            throw new LabServiceException("Received null in " + this + " from " + originalUrl);
+
+        // Replica
+        String replicaUrl = getReplicaNodeUri(request.getId()) + "/user";
+        if (null == restTemplate.postForObject(replicaUrl, httpRequest, User.class))
+            throw new LabServiceException("Received null in " + this + " from " + originalUrl);
+
+        return user;
     }
 
     public User createEntity(UserCreateRequest request) throws LabServiceException {
-        HttpEntity httpRequest = new HttpEntity<UserCreateRequest>(request, headers);
+        HttpEntity<UserCreateRequest> httpRequest = new HttpEntity<>(request, headers);
 
-        String url = currentNodeUrl(true) + "/createUser";
+        // Main
+        String originalUrl = getOriginalNodeUri(request.getId()) + "/createUser";
+        User user = restTemplate.postForObject(originalUrl, httpRequest, User.class);
+        if (user == null)
+            throw new LabServiceException("Received null in " + this + " from " + originalUrl);
 
-        // Main insert
+        // Replica
+        String replicaUrl = getReplicaNodeUri(request.getId()) + "/createUser";
+        if (null == restTemplate.postForObject(replicaUrl, httpRequest, User.class))
+            throw new LabServiceException("Received null in " + this + " from " + originalUrl);
+
+        return user;
+    }
+
+    public User updateEntity(UserUpdateRequest request) throws LabServiceException {
+        HttpEntity<UserUpdateRequest> httpRequest = new HttpEntity<>(request, headers);
+
+        String url = getOriginalNodeUri(request.getId()) + "/updateUser";
+
+        // Main
         User user = restTemplate.postForObject(url, httpRequest, User.class);
         if (user == null)
             throw new LabServiceException("Received null in " + this + " from " + url);
 
         // Replica
-        String replicaUrl = nextNodeUrl() + "/createUser";
+        String replicaUrl = getReplicaNodeUri(request.getId()) + "/createUser";
         if (null == restTemplate.postForObject(replicaUrl, httpRequest, User.class))
             throw new LabServiceException("Received null in " + this + " from " + url);
 
         return user;
     }
 
-    // TODO for all connected nodes
-    public User updateEntity(UserUpdateRequest request) throws LabServiceException {
-        return sendRequest("/updateUser", request);
+    public User deleteEntity(UserDeleteRequest request) throws LabServiceException {
+        HttpEntity<UserDeleteRequest> httpRequest = new HttpEntity<>(request, headers);
+
+        String url = getOriginalNodeUri(request.getId()) + "/updateUser";
+
+        // Main
+        User user = restTemplate.postForObject(url, httpRequest, User.class);
+        if (user == null)
+            throw new LabServiceException("Received null in " + this + " from " + url);
+
+        // Replica
+        String replicaUrl = getReplicaNodeUri(request.getId()) + "/createUser";
+        if (null == restTemplate.postForObject(replicaUrl, httpRequest, User.class))
+            throw new LabServiceException("Received null in " + this + " from " + url);
+
+        return user;
     }
 
-    // TODO for all connected nodes
-    public User deleteEntity(UserDeleteRequest request) throws LabServiceException {
-        return sendRequest("/deleteUser", request);
+    private String getOriginalNodeUri(Long requestId) {
+        updateNodeList();
+        int nodeId = (int) (requestId % nodes.size());
+        return nodes.get(nodeId).getUri().toString();
+    }
+
+    private String getReplicaNodeUri(Long requestId) {
+        updateNodeList();
+        int nodeId = (int) (++requestId % nodes.size());
+        return nodes.get(nodeId).getUri().toString();
     }
 
     private void updateNodeList() {
@@ -115,10 +152,10 @@ public class NodeService {
             List<ServiceInstance> serviceInstances = discoveryClient.getInstances("node");
 
             if (serviceInstances != null && !serviceInstances.isEmpty()) {
-                serviceInstances.sort((arg0, arg1) -> Integer.compare(arg0.getPort(), arg1.getPort()));
+                serviceInstances.sort(Comparator.comparingInt(ServiceInstance::getPort));
                 nodes = serviceInstances;
-                if (counter >= nodes.size())
-                    counter = 0;
+//                if (counter >= nodes.size())
+//                    counter = 0;
             } else {
                 throw new LabServiceException("No one node-server is connected");
             }
