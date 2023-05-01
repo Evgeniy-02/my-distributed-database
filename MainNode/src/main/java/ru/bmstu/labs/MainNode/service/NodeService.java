f1eed685eaeeed4b3db30be60d1892476b19755f
Svetlana <pirozhkova@ecopsy.ru>
2023-05-01 22:38:12 +0300

    Logs added. Methods implemented

--- MainNode/src/main/java/ru/bmstu/labs/MainNode/service/NodeService.java
+++ MainNode/src/main/java/ru/bmstu/labs/MainNode/service/NodeService.java
@@ -1,22 +1,21 @@
 package ru.bmstu.labs.MainNode.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import ru.bmstu.labs.MainNode.dto.user.UserCreateRequest;
-import ru.bmstu.labs.MainNode.dto.user.UserDeleteRequest;
-import ru.bmstu.labs.MainNode.dto.user.UserGetRequest;
-import ru.bmstu.labs.MainNode.dto.user.UserUpdateRequest;
+import ru.bmstu.labs.MainNode.dto.user.*;
 import ru.bmstu.labs.MainNode.issue.LabServiceException;
 import ru.bmstu.labs.MainNode.model.User;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class NodeService {
@@ -44,102 +43,145 @@ public class NodeService {
     }
 
     public List<User> getEntities(String alias) {
+        log.debug("method=getEntities message='Get users request received: alias={}'", alias);
+
         updateNodeList();
 
         List<User> resultList = new ArrayList<>();
+        List<User> users = new ArrayList<>();
 
         for (ServiceInstance node : nodes) {
-            HttpEntity<String> httpRequest = new HttpEntity<>(alias, headers);
+            HttpEntity<String> getEntitiesRequest = new HttpEntity<>(alias, headers);
 
-            String url = node.getUri().toString() + "/users";
-            User[] users = restTemplate.postForObject(url, httpRequest, User[].class);
+            String url = node.getUri().toString() + GET_ENTITIES_PATH;
+            ResponseEntity<List<User>> nodeUsersResponse = restTemplate.exchange(url, HttpMethod.POST, getEntitiesRequest, new ParameterizedTypeReference<List<User>>() {
+            });
+            List<User> nodeUsers = nodeUsersResponse.getBody();
 
-            resultList.addAll(List.of(users != null ? users : new User[0]));
+            log.debug("method=getEntities message='User for node {} requested: {}'", node.getUri(), nodeUsers);
+            if (nodeUsers != null) {
+                users.addAll(nodeUsers);
+            }
         }
 
+        users.sort(Comparator.comparing(User::getId));
+
+        for (int i = 0; i < users.size() - 1; i++) {
+            if (Objects.equals(users.get(i).getId(), users.get(i + 1).getId())) {
+                User user = getLastVersion(users.get(i), users.get(i + 1));
+
+                syncEntity(user);
+
+                i++;
+                if (user != null && user.getDeletedAt() == null) {
+                    resultList.add(user);
+                }
+            }
+        }
+
+        log.debug("method=getEntities message='Users requested successfully: {}'", resultList);
         return resultList;
     }
 
-    public User getEntity(UserGetRequest request) throws LabServiceException {
+    public User getEntity(UserGetRequest request) {
+        log.debug("method=getEntity message='Get user request received: request={}'", request);
         HttpEntity<UserGetRequest> httpRequest = new HttpEntity<>(request, headers);
 
         // Main
-        String originalUrl = getOriginalNodeUri(request.getId()) + "/user";
-        User user = restTemplate.postForObject(originalUrl, httpRequest, User.class);
-        if (user == null)
-            throw new LabServiceException("Received null in " + this + " from " + originalUrl);
+        String originalUrl = getOriginalNodeUri(request.getId()) + GET_ENTITY_PATH;
+        User originalUser = restTemplate.postForObject(originalUrl, httpRequest, User.class);
+        log.debug("method=getEntity message='Original user requested successfully: {}'", originalUser);
 
         // Replica
-        String replicaUrl = getReplicaNodeUri(request.getId()) + "/user";
-        if (null == restTemplate.postForObject(replicaUrl, httpRequest, User.class))
-            throw new LabServiceException("Received null in " + this + " from " + originalUrl);
+        String replicaUrl = getReplicaNodeUri(request.getId()) + GET_ENTITY_PATH;
+        User replicaUser = restTemplate.postForObject(replicaUrl, httpRequest, User.class);
+        log.debug("method=getEntity message='Replica user requested successfully: {}'", replicaUser);
 
+        User user = getLastVersion(originalUser, replicaUser);
+
+        syncEntity(user);
+
+        log.debug("method=getEntity message='User requested successfully: {}'", user);
+        if (user != null && user.getDeletedAt() != null) {
+            return null;
+        }
         return user;
     }
 
-    public User createEntity(UserCreateRequest request) throws LabServiceException {
+    public User createEntity(UserCreateRequest request) {
+        log.debug("method=createEntity message='Create user request received: request={}'", request);
         HttpEntity<UserCreateRequest> httpRequest = new HttpEntity<>(request, headers);
 
         // Main
-        String originalUrl = getOriginalNodeUri(request.getId()) + "/createUser";
-        User user = restTemplate.postForObject(originalUrl, httpRequest, User.class);
-        if (user == null)
-            throw new LabServiceException("Received null in " + this + " from " + originalUrl);
+        String originalUrl = getOriginalNodeUri(request.getId()) + CREATE_ENTITY_PATH;
+        User originalUser = restTemplate.postForObject(originalUrl, httpRequest, User.class);
+        log.debug("method=createEntity message='Original user created successfully: {}'", originalUser);
 
         // Replica
-        String replicaUrl = getReplicaNodeUri(request.getId()) + "/createUser";
-        if (null == restTemplate.postForObject(replicaUrl, httpRequest, User.class))
-            throw new LabServiceException("Received null in " + this + " from " + originalUrl);
+        String replicaUrl = getReplicaNodeUri(request.getId()) + CREATE_ENTITY_PATH;
+        User replicaUser = restTemplate.postForObject(replicaUrl, httpRequest, User.class);
+        log.debug("method=createEntity message='Replica user created successfully: {}'", replicaUser);
 
-        return user;
+        log.debug("method=createEntity message='User created successfully: {}'", originalUser);
+        return originalUser;
     }
 
-    public User updateEntity(UserUpdateRequest request) throws LabServiceException {
+    public User updateEntity(UserUpdateRequest request) {
+        log.debug("method=updateEntity message='Update user request received: request={}'", request);
         HttpEntity<UserUpdateRequest> httpRequest = new HttpEntity<>(request, headers);
 
-        String url = getOriginalNodeUri(request.getId()) + "/updateUser";
-
         // Main
-        User user = restTemplate.postForObject(url, httpRequest, User.class);
-        if (user == null)
-            throw new LabServiceException("Received null in " + this + " from " + url);
+        String url = getOriginalNodeUri(request.getId()) + UPDATE_ENTITY_PATH;
+        User originalUser = restTemplate.postForObject(url, httpRequest, User.class);
+        log.debug("method=updateEntity message='Original user updated successfully: {}'", originalUser);
 
         // Replica
-        String replicaUrl = getReplicaNodeUri(request.getId()) + "/createUser";
-        if (null == restTemplate.postForObject(replicaUrl, httpRequest, User.class))
-            throw new LabServiceException("Received null in " + this + " from " + url);
+        String replicaUrl = getReplicaNodeUri(request.getId()) + UPDATE_ENTITY_PATH;
+        User replicaUser = restTemplate.postForObject(replicaUrl, httpRequest, User.class);
+        log.debug("method=updateEntity message='Replica user updated successfully: {}'", replicaUser);
 
-        return user;
+        log.debug("method=updateEntity message='User updated successfully: {}'", originalUser);
+        return originalUser;
     }
 
-    public User deleteEntity(UserDeleteRequest request) throws LabServiceException {
+    public User deleteEntity(UserDeleteRequest request) {
+        log.debug("method=deleteEntity message='Delete user request received: request={}'", request);
         HttpEntity<UserDeleteRequest> httpRequest = new HttpEntity<>(request, headers);
 
-        String url = getOriginalNodeUri(request.getId()) + "/updateUser";
-
         // Main
-        User user = restTemplate.postForObject(url, httpRequest, User.class);
-        if (user == null)
-            throw new LabServiceException("Received null in " + this + " from " + url);
+        String url = getOriginalNodeUri(request.getId()) + DELETE_ENTITY_PATH;
+        User originalUser = restTemplate.postForObject(url, httpRequest, User.class);
+        log.debug("method=deleteEntity message='Original user deleted successfully: {}'", originalUser);
 
         // Replica
-        String replicaUrl = getReplicaNodeUri(request.getId()) + "/createUser";
-        if (null == restTemplate.postForObject(replicaUrl, httpRequest, User.class))
-            throw new LabServiceException("Received null in " + this + " from " + url);
+        String replicaUrl = getReplicaNodeUri(request.getId()) + DELETE_ENTITY_PATH;
+        User replicaUser = restTemplate.postForObject(replicaUrl, httpRequest, User.class);
+        log.debug("method=deleteEntity message='Replica user deleted successfully: {}'", replicaUser);
 
-        return user;
+        log.debug("method=deleteEntity message='User deleted successfully: {}'", originalUser);
+        return originalUser;
     }
 
     private String getOriginalNodeUri(Long requestId) {
+        log.debug("method=getOriginalNodeUri message='Original node uri requested: requestId={}'", requestId);
+
         updateNodeList();
         int nodeId = (int) (requestId % nodes.size());
-        return nodes.get(nodeId).getUri().toString();
+        String originalNodeUri = nodes.get(nodeId).getUri().toString();
+
+        log.debug("method=getOriginalNodeUri message='Original node uri received successfully: {}'", originalNodeUri);
+        return originalNodeUri;
     }
 
     private String getReplicaNodeUri(Long requestId) {
+        log.debug("method=getReplicaNodeUri message='Replica node uri requested: requestId={}'", requestId);
+
         updateNodeList();
         int nodeId = (int) (++requestId % nodes.size());
-        return nodes.get(nodeId).getUri().toString();
+        String replicaNodeUri = nodes.get(nodeId).getUri().toString();
+
+        log.debug("method=getReplicaNodeUri message='Replica node uri received successfully: {}'", replicaNodeUri);
+        return replicaNodeUri;
     }
 
     private void updateNodeList() {
@@ -154,6 +196,81 @@ public class NodeService {
             }
         } catch (LabServiceException e) {
             e.printStackTrace();
+        }
+    }
+
+    private User getLastVersion(User firstUser, User secondUser) {
+        log.debug("method=getLastVersion message='Last version requested between: firstUser={}, secondUser={}'",
+                firstUser, secondUser);
+
+        if (firstUser == null && secondUser == null) {
+            return null;
+        }
+
+        if (firstUser == null) {
+            return secondUser;
+        }
+        if (secondUser == null) {
+            return firstUser;
+        }
+
+        // check deleted at field
+        if (!Objects.equals(firstUser.getDeletedAt(), secondUser.getDeletedAt())) {
+            if (firstUser.getDeletedAt() == null) {
+                return secondUser;
+            }
+            if (secondUser.getDeletedAt() == null) {
+                return firstUser;
+            }
+
+            return firstUser.getDeletedAt() > secondUser.getDeletedAt() ? firstUser : secondUser;
+        }
+
+        // check updated at field
+        if (!Objects.equals(firstUser.getUpdatedAt(), secondUser.getUpdatedAt())) {
+            if (firstUser.getUpdatedAt() == null) {
+                return secondUser;
+            }
+            if (secondUser.getUpdatedAt() == null) {
+                return firstUser;
+            }
+
+            return firstUser.getUpdatedAt() > secondUser.getUpdatedAt() ? firstUser : secondUser;
+        }
+
+        // check created at field
+        if (!Objects.equals(firstUser.getCreatedAt(), secondUser.getCreatedAt())) {
+            if (firstUser.getCreatedAt() == null) {
+                return secondUser;
+            }
+            if (secondUser.getCreatedAt() == null) {
+                return firstUser;
+            }
+
+            return firstUser.getCreatedAt() > secondUser.getCreatedAt() ? firstUser : secondUser;
+        }
+
+        return firstUser;
+    }
+
+    private void syncEntity(User user) {
+        log.debug("method=syncEntity message='Synchronization for user requested: {}'", user);
+
+        if (user != null) {
+            UserRequest userRequest = new UserRequest();
+
+            userRequest.setId(user.getId());
+            userRequest.setName(user.getName());
+            userRequest.setLastname(user.getLastName());
+            userRequest.setEmail(user.getEmail());
+            userRequest.setCreatedAt(user.getCreatedAt());
+            userRequest.setUpdatedAt(user.getUpdatedAt());
+            userRequest.setDeletedAt(user.getDeletedAt());
+
+            HttpEntity<UserRequest> syncRequest = new HttpEntity<>(userRequest, headers);
+
+            restTemplate.postForObject(getOriginalNodeUri(user.getId()) + SYNC_PATH, syncRequest, User.class);
+            restTemplate.postForObject(getReplicaNodeUri(user.getId()) + SYNC_PATH, syncRequest, User.class);
         }
     }
 }
